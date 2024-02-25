53215d8acfa8aaeac54aaea9c25a1f3bb31719aa
Jamila Ali <ai241228@debi.eui.edu.eg>
2024-02-25 14:00:06 +0000

    Last version

--- hello.py
+++ hello.py
@@ -1,6 +1,20 @@
-def add(x, y):
-    return x + y
+# def add(x, y):
+#     return x + y
 
-#var=
-result = add(1, 2)
-print(f"This is the sum: {1}, {2}, {result}")
+# #var=
+# result = add(1, 2)
+# print(f"This is the sum: {1}, {2}, {result}")
+
+#Updated#
+
+#var=1
+#var=var
+
+def toyou(x):
+    return f"hi {x}"
+
+def add(x):
+    return x + 1
+
+def subtract(x):
+    return x - 1
